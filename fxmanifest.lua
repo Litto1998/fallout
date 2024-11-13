97920f2f672718091940cda79d3b1f2cebfaf59c
Litto1998 <73197083+Litto1998@users.noreply.github.com>
2024-11-13 11:50:00 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Your Name'
+description 'Fallout Style Menu for FiveM'
+version '1.0.0'
+
+client_scripts {
+    'client/main.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/styles.css',
+    'html/script.js',
+    'html/background.jpg',
+    'html/header.png'
+}
