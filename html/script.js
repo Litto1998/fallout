9274736da6c9be1b6c73147fd7f952380c7da162
Vincent Acanfora <brodino96@gmail.com>
2024-11-14 12:15:52 +0100

    chore(;): Removed all semicolons

--- html/script.js
+++ html/script.js
@@ -1,61 +1,61 @@
 // script.js
 $(document).ready(function() {
-    let selectedIndex = 0;
-    const menuItems = $('.menu-item');
+    let selectedIndex = 0
+    const menuItems = $('.menu-item')
     
     window.addEventListener('message', function(event) {
         if (event.data.action === "showMenu") {
             if (event.data.show) {
-                $('body').fadeIn(500);
+                $('body').fadeIn(500)
             } else {
-                $('body').fadeOut(500);
+                $('body').fadeOut(500)
             }
         }
-    });
+    })
 
     $(document).keydown(function(e) {
         switch(e.which) {
             /*
             case 38: // up arrow
-                selectedIndex = (selectedIndex - 1 + menuItems.length) % menuItems.length;
-                updateSelection();
-                break;
+                selectedIndex = (selectedIndex - 1 + menuItems.length) % menuItems.length
+                updateSelection()
+                break
             case 40: // down arrow
-                selectedIndex = (selectedIndex + 1) % menuItems.length;
-                updateSelection();
-                break;
+                selectedIndex = (selectedIndex + 1) % menuItems.length
+                updateSelection()
+                break
             case 13: // enter
-                handleMenuAction($(menuItems[selectedIndex]).data('action'));
-                break;
+                handleMenuAction($(menuItems[selectedIndex]).data('action'))
+                break
                 */
             case 27: // escape
-                closeMenu();
-                break;
+                closeMenu()
+                break
         }
-    });
+    })
 
     $('.menu-item').click(function() {
-        handleMenuAction($(this).data('action'));
-    });
+        handleMenuAction($(this).data('action'))
+    })
 
     function handleMenuAction(action) {
         switch(action) {
             case 'continue':
                 closeMenu()
-                break;
+                break
             case "exit":
                 closeMenu()
                 break
             default:
                 $.post(`https://${GetParentResourceName()}/menuAction`, JSON.stringify({
                     action: action
-                }));
+                }))
                 break
         }
     }
 
     function closeMenu() {
-        $.post(`https://${GetParentResourceName()}/closeMenu`, JSON.stringify({}));
-        $('body').fadeOut(500);
+        $.post(`https://${GetParentResourceName()}/closeMenu`, JSON.stringify({}))
+        $('body').fadeOut(500)
     }
-});
+})
