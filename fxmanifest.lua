28b696a62bf7774bf25450536d9e98a672065325
Vincent Acanfora <brodino96@gmail.com>
2024-11-14 12:16:47 +0100

    update(): Updated to reflect his usage

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,22 +1,22 @@
-fx_version 'cerulean'
-game 'gta5'
+fx_version "cerulean"
+game "gta5"
 lua54 "yes"
 
-author 'Your Name'
-description 'Fallout Style Menu for FiveM'
-version '1.0.0'
+author "Litto & Brodino"
+description "Pause menu for BlaineCounty"
+version "1.0.0"
 
 client_scripts {
-    'client/main.lua',
+    "client/main.lua",
     "client/nui.lua"
 }
 
-ui_page 'html/index.html'
+ui_page "html/index.html"
 
 files {
-    'html/index.html',
-    'html/styles.css',
-    'html/script.js',
-    'html/background.jpg',
-    'html/header.png'
+    "html/index.html",
+    "html/styles.css",
+    "html/script.js",
+    "html/background.jpg",
+    "html/header.png"
 }
